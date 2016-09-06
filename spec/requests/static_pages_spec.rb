1523ca17f56eed7dcdf07ddf1f47cc82b8fe8981
CzechChook <471798911@qq.com>
2016-09-06 16:37:35 +0800

    sanity testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,29 +4,60 @@ describe "Static pages" do
 	#define the subject 'page' should have.....
 	subject { page }
 
+	shared_examples_for "all static pages" do
+		it { should have_content(heading) }
+		#Here full_title() invoke /spec/support/utilities.rb 
+		it { should have_title(full_title(page_title))}
+	end	
+
 	describe "Home page" do
 		before{ visit '/' }
-		it { should have_content 'Sample App' }
-		it { should_not have_title '| Home' }
-		#Here full_title() invoke /spec/support/utilities.rb 
-		it { should have_title(full_title('')) }
+		#Here 'let' is a local variable it will appear when need it
+		#not like instance variable
+		let(:heading)   { 'Sample App' }
+		let(:page_title){ '' }
+		it_should_behave_like "all static pages"
+		it { should_not have_title('| Home') }
 	end
 	describe "Help page" do
 		before{ visit '/help' }
-		it { should have_content 'Help' }
-		#Here full_title() invoke /spec/support/utilities.rb 
-		it { should have_title(full_title('Help')) }
+		let(:heading)   { 'Help' }
+		let(:page_title){ 'Help' }
+		it_should_behave_like "all static pages"
 	end
 	describe "About page" do
 		before { visit '/about' }
-		it { should have_content 'About Us' }
-		#Here full_title() invoke /spec/support/utilities.rb 
-		it { should have_title(full_title('About Us')) }
+		let(:heading)   { 'About Us' }
+		let(:page_title){ 'About Us' }
+		it_should_behave_like "all static pages"
 	end
 	describe "Contact page" do
 	 	before { visit '/contact' }
-		it { should have_content 'Contact Us' }
-		#Here full_title() invoke /spec/support/utilities.rb 
-		it { should have_title(full_title('Contact Us')) }
+		let(:heading)   { 'Contact Us' }
+		let(:page_title){ 'Contact Us' }
+		it_should_behave_like "all static pages"
+	end
+
+
+	# Test right_links
+	it "should have the right links on the layout" do
+		visit '/'
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to have_title(full_title('Contact Us'))
+		click_link "Home"
+		expect(page).to have_title(full_title(''))
+		click_link "Sign up now!"
+		expect(page).to have_title(full_title('Sign Up'))
+		click_link "sample app"
+		expect(page).to have_title(full_title(''))
 	end
 end
+
+
+
+
+
